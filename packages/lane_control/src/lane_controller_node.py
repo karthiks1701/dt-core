69a6e2de9c6a2ea294dbeab33956fa9330b6acf5
cyrille TABE <cyrilletabe@live.fr>
2022-12-12 14:59:26 -0500

    add deadreckoning feedback and control to track a path

--- packages/lane_control/src/lane_controller_node.py
+++ packages/lane_control/src/lane_controller_node.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
+import math
 import rospy
+
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Quaternion, Twist, Pose, Point, Vector3, TransformStamped, Transform
+from duckietown_msgs.msg import WheelEncoderStamped
+import message_filters
+from tf2_ros import TransformBroadcaster
+from tf import transformations as tr
 
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from duckietown_msgs.msg import (
@@ -120,8 +128,187 @@ class LaneControllerNode(DTROS):
         self.sub_obstacle_stop_line = rospy.Subscriber(
             "~obstacle_distance_reading", StopLineReading, self.cbObstacleStopLineReading, queue_size=1
         )
+        
+        # introducing deadreckoning
+        self.state = 0
+        self.left_encoder_last = None
+        self.right_encoder_last = None
+        self.encoders_timestamp_last = None
+        self.encoders_timestamp_last_local = None
+        self.timestamp = None
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.yaw = 0.0
+        self.q = [0.0, 0.0, 0.0, 1.0]
+        self.tv = 0.0
+        self.rv = 0.0
 
-        self.log("Initialized!")
+        self.ticks_per_meter = 656.0
+        self.debug = False
+        self.wierd = True
+        self.wheelbase = 0.108
+        self.iter_ = 0
+        
+        #NOTE
+        #self.target_states = np.array([  [0.15,0.0], [0.3,0.0], [0.3,0.4]  ]) ---  left turn
+        self.target_states = np.array([  [0.12,0.0], [0.12,-0.3], [0.12,-0.4]  ]) #-- right turn
+        self.len_states = self.target_states.shape[0]
+        print(self.len_states)
+        self.final_state = 0 
+        
+        # subscirber
+        self.sub_encoder_left = message_filters.Subscriber("~left_wheel_encoder_node/tick", WheelEncoderStamped)
+        self.sub_encoder_right = message_filters.Subscriber("~right_wheel_encoder_node/tick", WheelEncoderStamped)
+        
+        self.ts_encoders = message_filters.ApproximateTimeSynchronizer(
+            [self.sub_encoder_left, self.sub_encoder_right], 1, 1
+        )
+            
+        #if path is created then only check for this 
+        # IF 
+        #self.callback(0,0)
+        self.alpha = 0.0
+        self.beta = 0.0
+        
+        self.log("Initialized controller")
+        
+    def reset_odometry(self):
+        self.state = 0
+        self.left_encoder_last = None
+        self.right_encoder_last = None
+        self.encoders_timestamp_last = None
+        self.encoders_timestamp_last_local = None
+        self.timestamp = None
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.yaw = 0.0
+        self.q = [0.0, 0.0, 0.0, 1.0]
+        self.tv = 0.0
+        self.rv = 0.0
+
+        self.ticks_per_meter = 656.0
+        self.debug = False
+        self.wierd = True
+        self.wheelbase = 0.108
+        self.iter_ = 0
+        self.final_state = 0
+    
+    def setwaypoint(self, waypoint):
+        self.target_states = waypoint
+    
+    def cb_ts_encoders(self, left_encoder, right_encoder):
+        timestamp_now = rospy.get_time()
+
+        # Use the average of the two encoder times as the timestamp
+        left_encoder_timestamp = left_encoder.header.stamp.to_sec()
+        right_encoder_timestamp = right_encoder.header.stamp.to_sec()
+        timestamp = (left_encoder_timestamp + right_encoder_timestamp) / 2
+
+        if not self.left_encoder_last:
+            self.left_encoder_last = left_encoder
+            self.right_encoder_last = right_encoder
+            self.encoders_timestamp_last = timestamp
+            self.encoders_timestamp_last_local = timestamp_now
+            return
+
+        # Skip this message if the time synchronizer gave us an older message
+        dtl = left_encoder.header.stamp - self.left_encoder_last.header.stamp
+        dtr = right_encoder.header.stamp - self.right_encoder_last.header.stamp
+        if dtl.to_sec() < 0 or dtr.to_sec() < 0:
+            self.loginfo("Ignoring stale encoder message")
+            return
+
+        left_dticks = left_encoder.data - self.left_encoder_last.data
+        right_dticks = right_encoder.data - self.right_encoder_last.data
+
+        left_distance = left_dticks * 1.0 / self.ticks_per_meter
+        right_distance = right_dticks * 1.0 / self.ticks_per_meter
+
+        # Displacement in body-relative x-direction
+        distance = (left_distance + right_distance) / 2
+
+        # Change in heading
+        dyaw = (right_distance - left_distance) / self.wheelbase
+
+        dt = timestamp - self.encoders_timestamp_last
+
+        if dt < 1e-6:
+            #self.logwarn("Time since last encoder message (%f) is too small. Ignoring" % dt)
+            dt = 1e-6
+            #return
+
+        self.tv = distance / dt
+        self.rv = dyaw / dt
+
+        if self.debug:
+            self.loginfo(
+                "Left wheel:\t Time = %.4f\t Ticks = %d\t Distance = %.4f m"
+                % (left_encoder.header.stamp.to_sec(), left_encoder.data, left_distance)
+            )
+
+            self.loginfo(
+                "Right wheel:\t Time = %.4f\t Ticks = %d\t Distance = %.4f m"
+                % (right_encoder.header.stamp.to_sec(), right_encoder.data, right_distance)
+            )
+
+            self.loginfo(
+                "TV = %.2f m/s\t RV = %.2f deg/s\t DT = %.4f" % (self.tv, self.rv * 180 / math.pi, dt)
+            )
+
+        dist = self.tv * dt
+        dyaw = self.rv * dt
+
+        self.yaw = self.angle_clamp(self.yaw + dyaw)
+        self.x = self.x + dist * math.cos(self.yaw)
+        self.y = self.y + dist * math.sin(self.yaw)
+        self.q = tr.quaternion_from_euler(0, 0, self.yaw)
+        self.timestamp = timestamp
+
+        self.left_encoder_last = left_encoder
+        self.right_encoder_last = right_encoder
+        self.encoders_timestamp_last = timestamp
+        self.encoders_timestamp_last_local = timestamp_now
+
+        if self.wierd and self.final_state == 0:
+            print("iterator ",self.iter_)
+            start_time = rospy.Time.now()
+            
+            self.state = 0
+
+            car_control_msg = Twist2DStamped()
+            car_control_msg.header.stamp = rospy.Time.now()
+            car_control_msg.header.seq = 0
+
+        # Add commands to car message
+            #vpx = 0.5*(self.target_states[self.iter_][0]-self.x - self.alpha)
+            #vpy = 0.06*(self.target_states[self.iter_][1]-self.y)
+            car_control_msg.v = 0.1
+            car_control_msg.omega = self.compute_omega(self.target_states[self.iter_],self.x,self.y,self.yaw,dt)
+            #= vpy/self.alpha
+            
+                
+            print( " car commands  ",car_control_msg.omega)
+            print("car control given",car_control_msg)
+
+            if self.check_point( np.array([self.x,self.y]),self.target_states[self.iter_] ):
+                    self.iter_ += 1
+                    car_control_msg.v = 0.0
+
+                    if self.iter_ == len(self.target_states):
+                        self.final_state = 1 # DONE
+                        car_control_msg.v = 0
+                        car_control_msg.omega = 0
+                        self.publishCmd(car_control_msg)
+                        #self.reset_odometry()
+                        return 
+
+            print("target_state_ ",self.target_states[self.iter_])
+            self.publishCmd(car_control_msg)
+            print("cur state ",self.x,self.y,self.yaw)
+            self.state = 1
+
 
     def cbObstacleStopLineReading(self, msg):
         """
@@ -229,30 +416,83 @@ class LaneControllerNode(DTROS):
                 omega = omega * 0.25
 
             else:
-                v, omega = self.controller.compute_control_action(
-                    d_err, phi_err, dt, wheels_cmd_exec, self.stop_line_distance
-                )
+                #v, omega = self.controller.compute_control_action(
+                #    d_err, phi_err, dt, wheels_cmd_exec, self.stop_line_distance
+                #)
+                self.ts_encoders.registerCallback(self.cb_ts_encoders)
 
             # For feedforward action (i.e. during intersection navigation)
             self.params["~omega_ff"] = rospy.get_param("~omega_ff", None)
-            rospy.loginfo("current computed omega %s sent omega_ff %s",str(omega), str(self.params["~omega_ff"]))
-            omega += self.params["~omega_ff"]
+            #rospy.loginfo("current computed omega %s sent omega_ff %s",str(omega), str(self.params["~omega_ff"]))
+            #omega += self.params["~omega_ff"]
 
         # Initialize car control msg, add header from input message
         car_control_msg = Twist2DStamped()
         car_control_msg.header = pose_msg.header
 
         # Add commands to car message
-        car_control_msg.v = v
-        car_control_msg.omega = omega
+        #car_control_msg.v = v
+        #car_control_msg.omega = omega
 
-        self.publishCmd(car_control_msg)
+        #self.publishCmd(car_control_msg)
         self.last_s = current_s
 
     def cbParametersChanged(self):
         """Updates parameters in the controller object."""
 
         self.controller.update_parameters(self.params)
+    
+    @staticmethod
+    def angle_clamp(theta):
+        if theta > 2 * math.pi:
+            return theta - 2 * math.pi
+        elif theta < -2 * math.pi:
+            return theta + 2 * math.pi
+        else:
+            return theta      
+
+    def path_plan(self,obstacle,lane):
+            return 0
+
+    def compute_omega(self,targetxy,x,y,current,dt):
+        factor = 1 # PARAM 
+
+        print("compute omega targetxy", targetxy)
+        print("compute omega current",x,y)
+
+        target_yaw = np.arctan2( (targetxy[1] - y),(targetxy[0]- x) )
+
+        print("target yaw ", np.rad2deg(target_yaw))
+        print("currnt_yaw ", np.rad2deg(current))
+
+        omega = factor* ((target_yaw - current))
+
+        return omega
+
+    def check_point(self,current_point,target_point):
+        threshold = 0.1
+        threshold_x = 0.08
+        dist_x = np.zeros((1,2))
+        dist_x[0,0] = (current_point[0]-self.alpha) - target_point[0]
+        dist_x[0,1] = (current_point[1]) - target_point[1]
+        if self.iter_ == (self.len_states - 1):
+
+            if abs(dist_x[0,1]) < threshold_x:
+                return True
+
+            return False
+
+        else:
+            dist = np.sqrt(((current_point[0]-self.alpha) - target_point[0])**2 + ((current_point[1]-self.alpha) - target_point[1])**2 )
+            print("check if we reached ")
+            print("dist ",dist)
+            print(" current and target point",current_point,target_point)
+            print("-"*10)
+
+            if (abs(dist_x[0,0])) > threshold_x or (dist) < threshold:
+                return True
+
+            return False
 
 
 if __name__ == "__main__":
